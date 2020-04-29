5b3abc0e6e15a89bfc2d1fd72eaad410d9fa9194
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2020-04-29 16:55:21 +0200

    Modied header, created component home

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { HomeComponent } from './main/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
