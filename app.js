28b2f72168f7d12ef3178d714724f913cecb9d9b
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2020-05-03 15:07:53 +0200

    Server Heroku 6

--- app.js
+++ app.js
@@ -4,22 +4,30 @@ const path = require("path");
 const compression = require("compression");
 const zlib = require("zlib");
 const helmet = require("helmet");
+const cluster = require("cluster");
 
-app.use(
-	compression({
-		level: zlib.Z_BEST_COMPRESSION,
-		strategy: zlib.Z_DEFAULT_STRATEGY,
-	})
-);
-app.use(helmet());
-app.use(express.static("public"));
+if (cluster.isMaster) {
+	let sizeCpus = require("os").cpus().length;
+	for (let i = 0; i < sizeCpus; i++) {
+		cluster.fork();
+	}
+} else {
+	app.use(
+		compression({
+			level: zlib.Z_BEST_COMPRESSION,
+			strategy: zlib.Z_DEFAULT_STRATEGY,
+		})
+	);
+	app.use(helmet());
+	app.use(express.static("public"));
 
-app.all("*", (_, res) => {
-	res.sendFile(path.join(__dirname, "public", "index.html"));
-	res.flush();
-});
+	app.all("*", (_, res) => {
+		res.sendFile(path.join(__dirname, "public", "index.html"));
+		res.flush();
+	});
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-	console.log(`server working on ${PORT}`);
-});
+	const PORT = process.env.PORT || 8080;
+	app.listen(PORT, () => {
+		console.log(`server working on ${PORT}`);
+	});
+}
