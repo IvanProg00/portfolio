28c0cb85802f352da9ad582780a0b1ebcbd98d27
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2020-05-03 14:43:13 +0200

    Heroku server 6

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const compression = require("compression");
+const zlib = require("zlib");
+const helmet = require("helmet");
 
+app.use(
+	compression({
+		level: zlib.Z_BEST_COMPRESSION,
+		strategy: zlib.Z_DEFAULT_STRATEGY,
+	})
+);
+app.use(helmet());
 app.use(express.static("public"));
-app.use(compression());
 
 app.all("*", (_, res) => {
 	res.sendFile(path.join(__dirname, "public", "index.html"));
+	res.flush();
 });
 
 const PORT = process.env.PORT || 8080;
