fdb0d227ebcdf1c587f92bd87ef064fa15552bd4
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2020-05-03 13:45:54 +0200

    Server For Heroku 3

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const compression = require("compression");
+
 app.use(express.static("public"));
+app.use(compression());
 
 app.all("*", (_, res) => {
 	res.sendFile(path.join(__dirname, "public", "index.html"));
