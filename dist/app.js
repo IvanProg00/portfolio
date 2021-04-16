82186ab7c865e5083550dc74eb3613deb1c7f54f
ivan-prog <gavrilovivan2000@gmail.com>
2021-04-16 14:00:27 +0200

    Build Frontend for server. Some changes made in backend.

--- dist/app.js
+++ dist/app.js
@@ -5,15 +5,16 @@ const path = require("path");
 const compression = require("compression");
 const cors = require("cors");
 const app = express();
-const PORT = parseInt(process.env.PORT ?? "") || 8080;
-const publicDir = path.join(__dirname, "..", "public");
+const PORT = parseInt(process.env.PORT ?? '') || 8080;
+const publicDir = path.join(__dirname, '..', 'public');
+const pagesDir = path.join(__dirname, '..', 'pages');
 app.use(compression());
 app.use(cors());
 app.use(express.static(publicDir));
-app.get("/", (_, res) => {
-    res.sendFile(path.join(publicDir, "index.html"));
+app.get('/', (_, res) => {
+    res.sendFile(path.join(pagesDir, 'index.html'));
 });
-app.all("*", (_, res) => {
+app.get('*', (_, res) => {
     res.redirect('/');
 });
 app.listen(PORT, () => {
--- server/app.ts
+++ server/app.ts
@@ -1,25 +1,25 @@
-// import express, { Express} from "express";
-import * as express from "express";
-import { Express, Request, Response } from "express";
-import * as path from "path";
-import * as compression from "compression";
-import * as cors from "cors";
+import * as express from 'express';
+import { Express, Request, Response } from 'express';
+import * as path from 'path';
+import * as compression from 'compression';
+import * as cors from 'cors';
 
 const app: Express = express();
-const PORT: number = parseInt(process.env.PORT ?? "") || 8080;
+const PORT: number = parseInt(process.env.PORT ?? '') || 8080;
 
-const publicDir: string = path.join(__dirname, "..", "public");
+const publicDir: string = path.join(__dirname, '..', 'public');
+const pagesDir: string = path.join(__dirname, '..', 'pages');
 
 app.use(compression());
 app.use(cors());
 app.use(express.static(publicDir));
 
-app.get("/", (_, res) => {
-	res.sendFile(path.join(publicDir, "index.html"));
+app.get('/', (_: Request, res: Response) => {
+	res.sendFile(path.join(pagesDir, 'index.html'));
 });
-app.all("*", (_:Request, res: Response) => {
-  res.redirect('/');
-})
+app.get('*', (_: Request, res: Response) => {
+	res.redirect('/');
+});
 
 app.listen(PORT, () => {
 	console.log(`Server is working on ${PORT}`);
